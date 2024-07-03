9addc99885da03e8a87dd27ab72db8506e4beeb6
mathgame2 <20twu@student.aisb.hu>
2024-07-03 11:31:53 +0100

    Adding done button
    Done button and some basic tracking of which items are chosen when having pressed done button

--- js/choosing_gear.js
+++ js/choosing_gear.js
@@ -43,12 +43,31 @@ window.addEventListener('DOMContentLoaded', function() {
         gridContainer.appendChild(entry);
     }
 
+    function addDoneButton(){
+      var gridContainer = document.getElementById('gear_list');
+      var itemWidth = 50;
+
+      var entry = document.createElement("img");
+      entry.tabIndex = totalGear;
+      entry.src = "../resources/done.png";
+      entry.alt = "done";
+      entry.className = 'doneButton';
+      entry.id = 'done';
+      entry.style.width = itemWidth + "%";
+      entry.setAttribute('nav-selectable', 'true');
+      entry.setAttribute('selected', 'false');
+
+      totalGear++;
+
+      gridContainer.appendChild(entry);
+  }
+
     add_new_gear("../resources/gear/rodandhook.png", "rodandhook");
     add_new_gear("../resources/gear/net.png", "net");
     add_new_gear("../resources/gear/spear.png", "spear");
     add_new_gear("../resources/gear/realfish1.png", "realfish1");
     add_new_gear("../resources/gear/realfish2.png", "realfish2");
-    add_new_gear("../resources/done.png", "done");
+    addDoneButton();
 
     (() => {
       const firstElement = document.querySelectorAll("[nav-selectable]")[0];
@@ -74,15 +93,13 @@ const selectElement = selectElement =>
     element.setAttribute("nav-selected", selectThisElement);
     element.setAttribute("nav-index", index);
     selectThisElement ? element.focus() : element.blur();
-    console.log(index + " : " + element.alt);
+    // console.log(index + " : " + element.alt);
   });
 
   const Down = event => {
     const allElements = getAllElements();
     const currentIndex = getTheIndexOfTheSelectedElement();
-    console.log(currentIndex);
     const goToFirstElement = currentIndex + 2 > allElements.length - 1;
-    console.log(currentIndex + 2);
     const setIndex = goToFirstElement ? 0 : currentIndex + 2;
     selectElement(allElements[setIndex] || allElements[0]);
   };
@@ -106,9 +123,7 @@ const selectElement = selectElement =>
   const Right = event => {
     const allElements = getAllElements();
     const currentIndex = getTheIndexOfTheSelectedElement();
-    console.log(currentIndex);
     const goToFirstElement = currentIndex + 1 > allElements.length - 1;
-    console.log(currentIndex + 2);
     const setIndex = goToFirstElement ? 0 : currentIndex + 1;
     selectElement(allElements[setIndex] || allElements[0]);
   };
@@ -117,11 +132,28 @@ const selectElement = selectElement =>
     const allElements = getAllElements();
     const currentIndex = getTheIndexOfTheSelectedElement();
     currentElement = allElements[currentIndex];
-    if(currentElement.getAttribute('selected') === 'true') {
+    if(currentElement.id === "done") {
+      const chosenGear = storeChosen();
+      console.log(chosenGear);
+    } else if(currentElement.getAttribute('selected') === 'true') {
       currentElement.setAttribute('selected', 'false');
     } else {
       currentElement.setAttribute('selected', 'true');
     }
+  }
+
+  function storeChosen() {
+    const allElements = getAllElements();
+    const chosenIds = [];
+    for (let index = 0; index < allElements.length; index++) {
+      const element = allElements[index]; 
+      if(element.getAttribute('selected') === 'true'){
+              chosenIds.push(parseInt(element.getAttribute("nav-index"), 10));
+            }
+
+    }
+    
+    return chosenIds;
   }
 
   var options = {
